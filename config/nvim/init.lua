7348529ab4ce76068f7c099cf02f844f34a28a5c
Trần Quang Mạnh <147424100+shoukoshy@users.noreply.github.com>
2024-08-20 18:46:21 +0700

    Update init.lua

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -3,6 +3,7 @@ vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.wrap = false
 vim.g.mapleader = ' '
+vim.opt.clipboard = "unnamedplus"
 
 vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
