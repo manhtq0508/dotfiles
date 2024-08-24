a6c7a669129bdadb34852658570959d993361298
manhtq <manhtq0508@gmail.com>
2024-08-24 16:37:51 +0700

    Config default fonts

--- config/polybar/update_layout.py
+++ config/polybar/update_layout.py
@@ -1,73 +1,79 @@
 #!/bin/python
 
-from pynput import keyboard as keybrd
-from pathlib import Path as path
+from pathlib import Path
+import os
 
-file = str(path("~/.config/polybar/layout.tmp").expanduser())
-layout = ["Tall", "Mirror Tall", "Full", "ThreeCol"]
-index = -1
+try:
+    from pynput import keyboard
+except ImportError:
+    os.system("echo pynput not isntalled >> ~/.config/polybar/layout.tmp")
+    os.exit(0)
 
-# Vì 1 lý do nào đó mà pynput tự bắt phím Alt trong tổ hợp Shift + Alt thành code <65511> 
-# thay vì 65513 của phím Alt nên phải xử lý thêm
-accept_keys = [ keybrd.Key.cmd,
-                keybrd.Key.shift,
-                keybrd.Key.ctrl,
-                keybrd.Key.alt,
-                keybrd.Key.space,
-                "<65511>" ]
+FILE = str(Path("~/.config/polybar/layout.tmp").expanduser())
+
+# pynput bắt phím alt trong tổ hợp Shift + Alt là <65511> nên phải xử lý thêm 1 trường hợp
+SPECIAL_ALT_KEYCODE = "<65511>"
+ACCEPT_MOD_KEYS = { keyboard.Key.cmd,
+                    keyboard.Key.ctrl,
+                    keyboard.Key.alt,
+                    keyboard.Key.shift,
+                    keyboard.Key.space }
+ACCEPT_WORKSPACE_KEYS =  { keyboard.KeyCode.from_char(str(i)) for i in range(1, 10) }
+
+LAYOUT = [ "Tall", "Mirror Tall", "Full", "ThreeCol" ]
+RESET_LAYOUT_VALUE = -1
+
+workspace = 0
+workspace_layout = [ RESET_LAYOUT_VALUE for i in range(9) ]
 current_keys = set()
 
-def write_layout():
-    global layout
-    global index
+def change_layout():
+    workspace_layout[workspace] = (workspace_layout[workspace] + 1) % len(LAYOUT)
+    
+    with open(FILE, "w", encoding="UTF-8") as f:
+        f.write(LAYOUT[workspace_layout[workspace]])
 
-    index = (index + 1) % len(layout)
-    with open(file, "w", encoding="UTF-8") as f:
-        f.write(layout[index])
+def change_workspace(ws_index: int):
+    global workspace
+    global workspace_layout
+
+    workspace = ws_index
+    workspace_layout[workspace] += RESET_LAYOUT_VALUE if workspace_layout[workspace] != -1 else 0
+    change_layout()
 
 def reset_layout():
-    global layout
-    global index
+    global workspace_layout
     
-    index = -1
-    write_layout()
+    workspace_layout[workspace] = RESET_LAYOUT_VALUE
+    change_layout()
 
-def on_key_press(key):
-    global current_keys
-    
+def key_press(key):
     keyStr = str(key)
-    if (key in accept_keys) or (keyStr in accept_keys):
-        if (keyStr == "<65511>"):
-            current_keys.add(keybrd.Key.alt)
-        else:
-            current_keys.add(key)
+    if keyStr == SPECIAL_ALT_KEYCODE:
+        current_keys.add(keyboard.Key.alt)
+    elif (key in ACCEPT_MOD_KEYS) or (key in ACCEPT_WORKSPACE_KEYS):
+        current_keys.add(key)
 
-    print(current_keys, len(current_keys))
-
-    if (keybrd.Key.cmd in current_keys) and (keybrd.Key.space in current_keys):
-        if len(current_keys) == 2:
-            write_layout()
-        elif (keybrd.Key.shift in current_keys) and (keybrd.Key.ctrl not in current_keys) and (keybrd.Key.alt not in current_keys):
+    if (keyboard.Key.cmd in current_keys) and (keyboard.Key.space in current_keys):
+        if len(current_keys) == 2: # cmd + space
+            change_layout() 
+        elif (keyboard.Key.shift in current_keys) and (keyboard.Key.ctrl not in current_keys) and (keyboard.Key.alt not in current_keys): # cmd + shift + space
             reset_layout()
+    elif (keyboard.Key.cmd in current_keys) and (keyboard.Key.alt not in current_keys) and (keyboard.Key.space not in current_keys) and (keyboard.Key.ctrl not in current_keys) and (keyboard.Key.space not in current_keys):
+        for i in ACCEPT_WORKSPACE_KEYS:
+            if i in current_keys:
+                change_workspace(int(str(i).replace("'", "")) - 1)
 
-def on_key_release(key):
-    global current_keys
-
-    if (str(key) != "<65511>") and (key not in current_keys):
-        return 
-
-    if str(key) == "<65511>":
-        current_keys.remove(keybrd.Key.alt)
-    else:
+def key_release(key):
+    if str(key) == SPECIAL_ALT_KEYCODE:
+        current_keys.remove(keyboard.Key.alt)
+    elif key in current_keys:
         current_keys.remove(key)
 
-    print(current_keys, len(current_keys))
-
-
 def main():
-    reset_layout() # Init layout
+    reset_layout()
 
-    with keybrd.Listener(on_press=on_key_press, on_release=on_key_release) as listener:
+    with keyboard.Listener(on_press=key_press, on_release=key_release) as listener:
         listener.join()
 
 if __name__ == "__main__":
